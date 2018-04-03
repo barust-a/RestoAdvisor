cfba257ccc53d87c3c6831ba7a20d0238d40a693
barust-a <barust_a@etna-alternance.net>
2018-04-03 15:18:37 +0200

    get resto via retrofit en log

--- app/src/main/java/com/example/alex/restoadvisor/MainActivity.java
+++ app/src/main/java/com/example/alex/restoadvisor/MainActivity.java
@@ -3,15 +3,36 @@ package com.example.alex.restoadvisor;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.restoadvisor.MESSAGE";
+    private static RestaurantAPI restaurantApi;
+    private List<Restaurant> restaurants;
+    private Retrofit retrofit;
+    private final String TAG = "MainActivity";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        this.configureRetrofit();
+
+        this.getRestaurantsViaAPI();
     }
 
     /** Called when the user taps the Send button */
@@ -22,6 +43,40 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
 
+    }
+    
+    private void configureRetrofit() {
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+        retrofit = new Retrofit.Builder().baseUrl("http://192.168.0.22:8000/api/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+         restaurantApi = retrofit.create(RestaurantAPI.class);
+    }
+
+    private void getRestaurantsViaAPI() {
+        restaurantApi.getRestaurants().enqueue(new Callback<List<Restaurant>>() {
+            @Override
+            public void onResponse(Call<List<Restaurant>> call, Response<List<Restaurant>> response) {
+                Log.d(TAG, "onResponse:");
+                List<Restaurant> restaurantList = response.body();
+                if (restaurantList != null) {
+                    for (Restaurant restaurant: restaurantList) {
+                        Log.d(TAG, "restaurant recu: " + restaurant.getId()+ " " + restaurant.getName());
+                    }
+
+                } else {
+                    Log.d(TAG, "onresponse: restaurant is empty " + response.body());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Restaurant>> call, Throwable t) {
+                Log.e(TAG, "onfailur: " + t.getMessage());
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/example/alex/restoadvisor/RestaurantAPI.java
@@ -0,0 +1,12 @@
+package com.example.alex.restoadvisor;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface RestaurantAPI {
+    @GET("restos")
+    Call<List<Restaurant>> getRestaurants();
+
+}
